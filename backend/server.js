ab102670b1f76ae4a5576ee7b6e75171ee2b13dd
ooiws <ooiwensong@gmail.com>
2023-11-23 23:07:16 +0800

    install cors and helmet

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 require("dotenv").config();
 
 const express = require("express");
+const connectDB = require("./src/db/db");
+const cors = require("cors");
+const helmet = require("helmet");
+const reportTypes = require("./src/routers/reportTypes");
+
+connectDB();
 
 const app = express();
 
+app.use(cors());
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/report_types", reportTypes);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
