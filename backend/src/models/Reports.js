3747f2e1576b181c222648234f176315f50ddae3
ooiws <ooiwensong@gmail.com>
2023-11-23 23:07:29 +0800

    create model for reports

---
+++ backend/src/models/Reports.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const UpdatesSchema = new mongoose.Schema({
+  type: { type: String, enum: ["receipt", "newLeads", "transfer", "close"] },
+  io: { type: String },
+  branch: { type: String },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const ReportsSchema = new mongoose.Schema(
+  {
+    RefId: { type: String },
+    type: { type: String, required: true },
+    primaryInfo: { type: Map, of: String, required: true },
+    secondaryInfo: { type: String },
+    media: {},
+    AddInfo: { type: String },
+    dateOccurred: { type: Date, required: true },
+    timeOccurred: { type: String, required: true },
+    locationOccurred: { type: String },
+    submittedBy: {
+      /**ObjecId */
+    },
+    submmitedOn: { type: Date, default: Date.now },
+    status: {
+      type: String,
+      enum: ["Pending", "Active", "Completed"],
+      default: "Pending",
+    },
+    updates: [UpdatesSchema],
+  },
+  { collection: "reports" }
+);
+
+module.exports = mongoose.model("Report", ReportsSchema);
