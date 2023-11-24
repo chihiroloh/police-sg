0d9d08bb11af88df62e62cdf3c983fa7e464c069
chihiroloh <108581702+chihiroloh@users.noreply.github.com>
2023-11-24 20:00:12 +0800

    home changes

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,18 +1,54 @@
-import React from "react";
-import logo from "../assets/logo.png";
+import React, { useState } from "react";
+import headerwithnoti from "../assets/headerwithnoti.png";
 import { useNavigate } from "react-router-dom";
+import "./Home.css";
+import crimebutton from "../assets/crimebutton.png";
 
 const Home = () => {
   const navigate = useNavigate();
+  const [showAnnouncements, setShowAnnouncements] = useState(true);
+
+  const toggleAnnouncements = () => {
+    setShowAnnouncements(!showAnnouncements);
+  };
 
   const goToReportACrime = () => {
     navigate("/ReportACrime");
   };
 
   return (
-    <div>
-      <img src={logo} alt="logo" style={{ maxWidth: "57%" }} />
-      <p></p>
+    <div className="content-container">
+      <img
+        className="header-image"
+        src={headerwithnoti}
+        alt="loheaderwithnoti"
+      />
+      <br />
+      <div className="home-container">
+        {showAnnouncements && (
+          <div className="announcement-column">
+            <div className="announcement-header">
+              <button className="close-button" onClick={toggleAnnouncements}>
+                x
+              </button>
+              <h3>Important Annoucements</h3>
+            </div>
+            <p>
+              The SPF would like to alert the public to remain vigilant against
+              investment scams. Scammers approach victims through social media
+              platforms... <b className="readmore">Read more</b>
+            </p>
+            <p>1d ago • 1 min read</p>
+          </div>
+        )}
+      </div>
+      <div>
+        <h2 className="welcome">Hello Hillary Ho Hui Hui,</h2>
+        <p className="help">How can we help you today?</p>
+      </div>
+      <button className="crimebutton" onClick={goToReportACrime}>
+        <img src={crimebutton} alt="crimebutton" />
+      </button>
       <button onClick={goToReportACrime}>Report a Crime</button>
     </div>
   );
