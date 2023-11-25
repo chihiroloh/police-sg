c14a7d77a0091cef3534e8ba0d95a3a08a143603
chihiroloh <108581702+chihiroloh@users.noreply.github.com>
2023-11-25 17:26:04 +0800

    navbar

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css"; // You can define your CSS styles for the navbar in this file
+import HomeImage from "../assets/navbar/home.png";
+import ContactUsImage from "../assets/navbar/contactus.png";
+import AppealsImage from "../assets/navbar/appeals.png";
+import NewsImage from "../assets/navbar/news.png";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <Link to="/">
+        <img src={HomeImage} alt="Home" />
+      </Link>
+      <Link to="/ContactUs">
+        <img src={ContactUsImage} alt="Contact Us" />
+      </Link>
+      <Link to="/Appeals">
+        <img src={AppealsImage} alt="Appeals" />
+      </Link>
+      <Link to="/News">
+        <img src={NewsImage} alt="News" />
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
