df763e168d249c07a0a5b8a102c05311ecbdbc75
chihiroloh <108581702+chihiroloh@users.noreply.github.com>
2023-11-27 20:56:25 +0800

    updates

--- frontend/src/components/AppealOne.jsx
+++ frontend/src/components/AppealOne.jsx
@@ -9,25 +9,24 @@ const AppealOne = () => {
   const contentsRef = useRef("");
   const createdateRef = useRef("");
 
+  const [appealData, setAppealData] = useState(null);
+
   const addAppeal = async () => {
     try {
-      const res = await fetch(
-        import.meta.env.VITE_SERVER + "/api/appeals/seed",
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + userInfoCtx.accessToken,
-          },
-          body: JSON.stringify({
-            name: nameRef.current.value,
-            type: appealTypeRef.current.value,
-            imageURL: imageRef.current.value,
-            contents: contentsRef.current.value,
-            createdAt: createdateRef.current.value,
-          }),
-        }
-      );
+      const res = await fetch(import.meta.env.VITE_SERVER + "/api/appeals", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + userInfoCtx.accessToken,
+        },
+        body: JSON.stringify({
+          name: nameRef.current.value,
+          type: appealTypeRef.current.value,
+          imageURL: imageRef.current.value,
+          contents: contentsRef.current.value,
+          createdAt: createdateRef.current.value,
+        }),
+      });
 
       if (res.ok) {
         nameRef.current.value = "";
@@ -35,6 +34,9 @@ const AppealOne = () => {
         imageRef.current.value = "";
         contentsRef.current.value = "";
         createdateRef.current.value = "";
+
+        // Fetch and update appeal data after adding
+        fetchAppealData();
       } else {
         const data = await res.json();
         alert(data.message);
@@ -44,21 +46,54 @@ const AppealOne = () => {
       console.error("Error:", error.message);
     }
   };
+
+  // Fetch appeal data function
+  const fetchAppealData = async () => {
+    try {
+      const res = await fetch(
+        // Replace with the correct API endpoint to fetch appeal data
+        import.meta.env.VITE_SERVER + "/api/appeals"
+      );
+
+      if (res.ok) {
+        const data = await res.json();
+        // Assuming you want to display the first appeal data
+        if (data.length > 0) {
+          setAppealData(data[0]);
+        }
+      } else {
+        console.error("Failed to fetch appeal data");
+      }
+    } catch (error) {
+      console.error("Error fetching appeal data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAppealData(); // Fetch initial appeal data when the component mounts
+  }, []);
+
   return (
     <div>
       <p>Appeal Information</p>
       <hr />
-      <div className="appeal-details">
-        <h3 className="appeal-name">{appealData.name}</h3>
-        <div className="appeal-info">
-          <p className="appeal-type">Type: {appealData.type}</p>
-          <p className="appeal-contents">{appealData.contents}</p>
-          <p className="appeal-createdat">Created At: {appealData.createdAt}</p>
+      {appealData ? (
+        <div className="appeal-details">
+          <h3 className="appeal-name">{appealData.name}</h3>
+          <div className="appeal-info">
+            <p className="appeal-type">Type: {appealData.type}</p>
+            <p className="appeal-contents">{appealData.contents}</p>
+            <p className="appeal-createdat">
+              Created At: {appealData.createdAt}
+            </p>
+          </div>
         </div>
-      </div>
+      ) : (
+        <p>Loading appeal data...</p>
+      )}
       <img
         className="appeal-image"
-        src={appealData.imageURL}
+        src={appealData?.imageURL}
         alt="Appeal Image"
       />
     </div>
