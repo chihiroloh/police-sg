0c4a20a3c636d94950143d9d90b08a598843c4b3
ooiws <ooiwensong@gmail.com>
2023-11-27 14:09:19 +0800

    amend app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,8 +18,8 @@ import News from "./components/News";
 import NewsContent from "./components/NewsContent";
 import AppealOne from "./components/AppealOne";
 import UserContext from "./contexts/user";
-
 import ViewUpdate from "./components/ViewUpdate";
+
 function App() {
   const [accessToken, setAccessToken] = useState("");
   const [userId, setUserId] = useState("");
@@ -166,150 +166,18 @@ function App() {
                 </Layout>
               }
             />
+            <Route
+              path="/ViewUpdate"
+              element={
+                <Layout>
+                  <ViewUpdate />
+                </Layout>
+              }
+            />
           </Routes>
         </Router>
       </UserContext.Provider>
     </>
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Layout>
-              <LoginPage />
-            </Layout>
-          }
-        />
-        <Route
-          path="/singpass"
-          element={
-            <Layout>
-              <Singpass />
-            </Layout>
-          }
-        />
-        <Route
-          path="/tutorial"
-          element={
-            <Layout>
-              <Tutorial />
-            </Layout>
-          }
-        />
-        <Route
-          path="/home"
-          element={
-            <Layout>
-              <Home />
-            </Layout>
-          }
-        />
-        <Route
-          path="/reportacrime"
-          element={
-            <Layout>
-              <ReportACrime />
-            </Layout>
-          }
-        />
-        <Route
-          path="/lostnfound"
-          element={
-            <Layout>
-              <LostNFound />
-            </Layout>
-          }
-        />
-        <Route
-          path="/casestatus"
-          element={
-            <Layout>
-              <CaseStatus />
-            </Layout>
-          }
-        />
-        <Route
-          path="/cop"
-          element={
-            <Layout>
-              <Cop />
-            </Layout>
-          }
-        />
-        <Route
-          path="/about"
-          element={
-            <Layout>
-              <About />
-            </Layout>
-          }
-        />
-        <Route
-          path="/toc"
-          element={
-            <Layout>
-              <Toc />
-            </Layout>
-          }
-        />
-        <Route
-          path="/privacy"
-          element={
-            <Layout>
-              <Privacy />
-            </Layout>
-          }
-        />
-        <Route
-          path="/contactus"
-          element={
-            <Layout>
-              <ContactUs />
-            </Layout>
-          }
-        />
-        <Route
-          path="/appeals"
-          element={
-            <Layout>
-              <Appeals />
-            </Layout>
-          }
-        />
-        <Route
-          path="/AppealOne"
-          element={
-            <Layout>
-              <AppealOne />
-            </Layout>
-          }
-        />
-        <Route
-          path="/News"
-          element={
-            <Layout>
-              <News />
-            </Layout>
-          }
-        />
-        <Route
-          path="/NewsContent"
-          element={
-            <Layout>
-              <NewsContent />
-            </Layout>
-          }
-        />
-        <Route
-          path="/ViewUpdate"
-          element={
-            <Layout>
-              <ViewUpdate />
-            </Layout>
-          }
-        />
-      </Routes>
-    </Router>
   );
 }
 
